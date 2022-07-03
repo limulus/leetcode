92d626de5095795c4b1d4d600aeaa46011860afa
Eric McCarthy <eric@limulus.net>
2022-07-02 17:45:19 -0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# @limulus/leetcode
+
+LeetCode Solutions
+
+## Installation
+
+```sh
+npm install @limulus/leetcode
+```
+
+## Usage
+
+```javascript
+import Class from '@limulus/leetcode'
+```
+
+## API
+
+### Class
+
+#### constructor
+
+```javascript
+const obj = new Class()
+```
+
+#### obj.method()
+
---
+++ jest.config.js
@@ -0,0 +1,28 @@
+export default {
+  coveragePathIgnorePatterns: ['node_modules', 'mocks'],
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['node_modules', 'dist'],
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2022',
+        },
+      },
+    ],
+  },
+}
